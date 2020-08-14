5ecb6b063e593cb2aaccf7661d0296813fc53e07
Brennen Garland <bgarland015@gmail.com>
2020-08-14 14:47:02 -0400

    successfully returns received power from target

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@
 use specs::prelude::*;
 use std::{thread, time};
 
-// A component contains data which is associated with an entity.
-// pos in meters
+
 #[derive(Debug)]
 struct Position {
-    x: f32,
+    x: f32, // meters
     y: f32,
     z: f32,
 }
@@ -28,6 +27,17 @@ impl Component for Targets {
     type Storage = VecStorage<Self>;
 }
 
+#[derive(Debug)]
+struct Sensor {
+    frequency: f32, // Hz
+    gain: f32,      // w / w
+    p_t: f32,       // Watts
+    lambda: f32,    // wavelength
+}
+impl Component for Sensor {
+    type Storage = VecStorage<Self>;
+}
+
 // m/s
 #[derive(Debug)]
 struct Velocity {
@@ -35,45 +45,38 @@ struct Velocity {
     y: f32,
     z: f32,
 }
-
 impl Component for Velocity {
     type Storage = VecStorage<Self>;
 }
 
-
+// Simulating the radar sensor
 struct RadarSensing;
 impl<'a> System<'a> for RadarSensing {
-    // These are the resources required for execution.
-    // You can also define a struct and `#[derive(SystemData)]`,
-    // see the `full` example.
     type SystemData = (
         ReadStorage<'a, Position>,
         ReadStorage<'a, Signature>,
-        WriteStorage<'a, Targets>,
+        ReadStorage<'a, Sensor>,
     );
 
-    fn run(&mut self, (pos, sig, mut targ): Self::SystemData) {
-        // The `.join()` combines multiple components,
-        // so we only access those entities which have
-        // both of them.
-        // You could also use `par_join()` to get a rayon `ParallelIterator`.
-        for (targ, radar_pos) in (&mut targ, &pos).join() {
-            for(targ_pos, sig) in (&pos, &sig).join() {
-                
-                let range = ((radar_pos.x - targ_pos.x).powi(2) + (radar_pos.y - targ_pos.y).powi(2)).sqrt();
+    fn run(&mut self, (pos, sig, sen): Self::SystemData) {
+        // Loops through entities with only a target list and posiiton
+        for (sen, radar_pos) in (&sen, &pos).join() {
+            // Loops through entities with only a position and signiture
+            for(pos, sig) in (&pos, &sig).join() {
+            
+            // Power received: Pr = (Pt * G^2 * lambda^2 * rcs) / ((4pi)^3 * R^4)
+            let r = ((radar_pos.x - pos.x).powi(2) + (radar_pos.y - pos.y).powi(2)).sqrt();
 
-                // // Begin range equation
-                // let c = 299792458.0;
-                // let d_t = 1.0;     // Delta t (s)
-                // let range = (c*d_t) / 2.0;
-                println!("\nTarget at {}m\n", range);
-                targ.targ_array.push(range);
+            let p_r = (sen.p_t * sen.gain.powi(2) * sig.0 * sen.lambda.powi(2) * 10.0_f32.powi(14)) / (1984.4017 * r.powi(4));
+
+            println!("Received Power: {}", p_r);
             }
 
         }
     }
 }
 
+// Changes the position of each entity with position and velocity
 struct Movement;
 impl<'a> System<'a> for Movement {
 
@@ -83,48 +86,54 @@ impl<'a> System<'a> for Movement {
     );
 
     fn run(&mut self, (mut position, velocity): Self::SystemData) {
+        let mut i = 1;
         for(pos, vel) in (&mut position, &velocity).join() {
             pos.x += vel.x;
             pos.y += vel.y;
-            println!("X: {}", pos.x);
+            println!("Target{}\nX: {}", i, pos.x);
             println!("Y: {}", pos.y);
+            i += 1;
         }
     }
 }
 
 fn main() {
-    // The `World` is our
-    // container for components
-    // and other resources.
 
     let mut world = World::new();
-
-    // This builds a dispatcher
-    // The third parameter of `add` specifies
-    // logical dependencies on other systems.
-    // Since we only have one, we don't depend on anything.
-    // See the `full` example for dependencies.
-    let mut dispatcher = DispatcherBuilder::new().with(RadarSensing, "radar_sensing", &[]).with(Movement, "movement", &[]).build();
-
-    // setup() must be called before creating any entity, it will register
-    // all Components and Resources that Systems depend on
+    let mut dispatcher = DispatcherBuilder::new().with(RadarSensing, "radar_sensing", &[]).build();
     dispatcher.setup(&mut world);
 
+    // INPUTS FOR RADAR SENSOR
+    
+    let p_t: f32 = 100.0;        // kW    
+    let gain = 32.0;                // dB
+    let frequency = 9400000000.0;  // Hz
+
+    // TARGET INFO
+    let rcs = 1.0;                 // m^2
+    let targ_x = 50000.0;          // m from sensor
+    let targ_y = 0.0;
+    let targ_z = 0.0;
+
     // An entity may or may not contain some component
-    // This entity does not have `Vel`, so it won't be dispatched.
-    let radar = world.create_entity().with(Position{x: 0.0, y: 0.0, z: 1.0}).with(Targets{targ_array: Vec::new()}).build();
-    let target = world.create_entity().with(Position{x: 10.0, y: 20.0, z: 1.0}).with(Signature{0: 10.0}).with(Velocity{x: 10.0, y: 10.0, z: 0.0}).build();
+    let _radar = world.create_entity().with(Position{x: 0.0, y: 0.0, z: 1.0})
+    .with(Sensor{
+        frequency: frequency, 
+        gain: 10.0_f32.powf(gain / 10.0), 
+        p_t: (p_t * 1000.0), 
+        lambda: ((3.0 * 100000000.0) / frequency),
+        }).build();
+    let _target1 = world.create_entity().with(Position{x: targ_x, y: targ_y, z: targ_z}).with(Signature{0: rcs}).build();
 
     // This dispatches all the systems in parallel (but blocking)
     let one_sec = time::Duration::from_secs(1);
     loop {
         let start = time::Instant::now();
         dispatcher.dispatch(&world);
-        world.maintain();
+        // Create frame_rate loop
         let sleep_time = one_sec.checked_sub(time::Instant::now().duration_since(start));
         if sleep_time != None {
             thread::sleep(sleep_time.unwrap());
         }
-        // thread::sleep(one_sec);
     }
 }
