0cfe2b341ccdf2f92d495b6e06c6b9cc07ab019a
Brennen <bgarland@gmail.com>
2021-01-29 14:14:25 -0500

    examples not working

--- src/functions/doppler_shift.rs
+++ src/functions/doppler_shift.rs
@@ -1,8 +1,10 @@
 use super::*;
 
+const C: f32 = 299792458.0;
+
 pub fn doppler_shift(vel: &Velocity, illum: &Illumination) -> f32 {
     let tot_vel = (vel.x.powi(2) + vel.y.powi(2) + vel.z.powi(2)).sqrt();
-    return (1.0 + (2.0 * (tot_vel / 300000000.0))) * illum.frequency; 
+    return (1.0 + (2.0 * (tot_vel / C))) * illum.frequency; 
 }
 
 #[cfg(test)]
